916231391d575fc20a1ac7259003e2fec8f54721
Swapnil Pande <swapnilpande@example.com>
2025-11-30 18:35:51 +0530

    Fix deployment: Add .gitignore, remove node_modules, add .nojekyll

--- ilms-frontend/webpack.config.js
+++ ilms-frontend/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.jsx',
@@ -33,6 +34,11 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'public/.nojekyll', to: '' },
+      ],
     }),
   ],
   devServer: {
